600e08051760a36149d2ef55fbc3b0c337f1a732
willchan8 <wchan1992@gmail.com>
2019-05-04 11:58:42 -0700

    Wrote middleware function for cookieParser

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,14 @@
 const parseCookies = (req, res, next) => {
+  var cookiesObj = {};
+  if (req.get('Cookie')) {
+    var cookiesArr = req.get('Cookie').split(';');
+    for (let i = 0; i < cookiesArr.length; i++) {
+      var cookie = cookiesArr[i].split('='); //[' otherCookie', 18ea4fb6ab3178092ce936c591ddbb90c99c9f66]
+      cookiesObj[cookie[0].trim()] = cookie[1].trim();
+    }
+  }
+  req.cookies = cookiesObj;
+  next();
 };
 
 module.exports = parseCookies;
